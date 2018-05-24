ffed4ce8902ba861588bbe36ea62b9cfad36902c
Wiezorek, Jan <jan.wiezorek@vitodata.ch>
2018-05-24 09:58:19 +0200

    add variation

--- src/app/ngx-mat-select-search/ngx-mat-select-search.component.ts
+++ src/app/ngx-mat-select-search/ngx-mat-select-search.component.ts
@@ -20,10 +20,15 @@ export class SelectComponent implements OnInit, OnDestroy, AfterViewInit {
 
 
   /** control for the selected bank */
-  public bankCtrl: FormControl = new FormControl();
+  public bankCtrl1: FormControl = new FormControl();
+  /** control for the selected bank */
+  public bankCtrl2: FormControl = new FormControl();
 
   /** control for the MatSelect filter keyword */
-  public bankFilterCtrl: FormControl = new FormControl();
+  public bankFilterCtrl1: FormControl = new FormControl();
+
+  /** control for the MatSelect filter keyword */
+  public bankFilterCtrl2: FormControl = new FormControl();
 
   /** control for the selected bank for multi-selection */
   public bankMultiCtrl: FormControl = new FormControl();
@@ -54,12 +59,14 @@ export class SelectComponent implements OnInit, OnDestroy, AfterViewInit {
   ];
 
   /** list of banks filtered by search keyword */
-  public filteredBanks: ReplaySubject<Bank[]> = new ReplaySubject<Bank[]>(1);
+  public filteredBanks1: ReplaySubject<Bank[]> = new ReplaySubject<Bank[]>(1);
+  public filteredBanks2: ReplaySubject<Bank[]> = new ReplaySubject<Bank[]>(1);
 
   /** list of banks filtered by search keyword for multi-selection */
   public filteredBanksMulti: ReplaySubject<Bank[]> = new ReplaySubject<Bank[]>(1);
 
-  @ViewChild('singleSelect') singleSelect: MatSelect;
+  @ViewChild('singleSelect1') singleSelect1: MatSelect;
+  @ViewChild('singleSelect2') singleSelect2: MatSelect;
   @ViewChild('multiSelect') multiSelect: MatSelect;
 
   /** Subject that emits when the component has been destroyed. */
@@ -67,19 +74,28 @@ export class SelectComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngOnInit() {
     // set initial selection
-    this.bankCtrl.setValue(this.banks[10]);
+    this.bankCtrl1.setValue(this.banks[10]);
+    this.bankCtrl2.setValue(this.banks[10]);
     this.bankMultiCtrl.setValue([this.banks[10], this.banks[11], this.banks[12]]);
 
     // load the initial bank list
-    this.filteredBanks.next(this.banks.slice());
+    this.filteredBanks1.next(this.banks.slice());
+    this.filteredBanks2.next(this.banks.slice());
     this.filteredBanksMulti.next(this.banks.slice());
 
     // listen for search field value changes
-    this.bankFilterCtrl.valueChanges
+    this.bankFilterCtrl1.valueChanges
       .pipe(takeUntil(this._onDestroy))
       .subscribe(() => {
-        this.filterBanks();
+        this.filterBanks1();
       });
+    // listen for search field value changes
+    this.bankFilterCtrl2.valueChanges
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe(() => {
+        this.filterBanks2();
+      });
+
     this.bankMultiFilterCtrl.valueChanges
       .pipe(takeUntil(this._onDestroy))
       .subscribe(() => {
@@ -101,7 +117,7 @@ export class SelectComponent implements OnInit, OnDestroy, AfterViewInit {
    * Sets the initial value after the filteredBanks are loaded initially
    */
   private setInitialValue() {
-    this.filteredBanks
+    this.filteredBanks1
       .pipe(take(1), takeUntil(this._onDestroy))
       .subscribe(() => {
         // setting the compareWith property to a comparison function
@@ -109,25 +125,55 @@ export class SelectComponent implements OnInit, OnDestroy, AfterViewInit {
         // the form control (i.e. _initializeSelection())
         // this needs to be done after the filteredBanks are loaded initially
         // and after the mat-option elements are available
-        this.singleSelect.compareWith = (a: Bank, b: Bank) => a.id === b.id;
+        this.singleSelect1.compareWith = (a: Bank, b: Bank) => a.id === b.id;
+        this.multiSelect.compareWith = (a: Bank, b: Bank) => a.id === b.id;
+      });
+
+      this.filteredBanks2
+      .pipe(take(1), takeUntil(this._onDestroy))
+      .subscribe(() => {
+        // setting the compareWith property to a comparison function
+        // triggers initializing the selection according to the initial value of
+        // the form control (i.e. _initializeSelection())
+        // this needs to be done after the filteredBanks are loaded initially
+        // and after the mat-option elements are available
+        this.singleSelect2.compareWith = (a: Bank, b: Bank) => a.id === b.id;
         this.multiSelect.compareWith = (a: Bank, b: Bank) => a.id === b.id;
       });
   }
 
-  private filterBanks() {
+  private filterBanks1() {
     if (!this.banks) {
       return;
     }
     // get the search keyword
-    let search = this.bankFilterCtrl.value;
+    let search = this.bankFilterCtrl1.value;
     if (!search) {
-      this.filteredBanks.next(this.banks.slice());
+      this.filteredBanks1.next(this.banks.slice());
       return;
     } else {
       search = search.toLowerCase();
     }
     // filter the banks
-    this.filteredBanks.next(
+    this.filteredBanks1.next(
+      this.banks.filter(bank => bank.name.toLowerCase().indexOf(search) > -1)
+    );
+  }
+
+  private filterBanks2() {
+    if (!this.banks) {
+      return;
+    }
+    // get the search keyword
+    let search = this.bankFilterCtrl1.value;
+    if (!search) {
+      this.filteredBanks2.next(this.banks.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // filter the banks
+    this.filteredBanks2.next(
       this.banks.filter(bank => bank.name.toLowerCase().indexOf(search) > -1)
     );
   }
